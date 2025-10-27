5881820820e195d53568402415cae98772ce80de
joaopbarcellos <jpzbarcellos@gmail.com>
2025-10-27 18:12:26 -0300

    Quase pronto

--- pthreads/main.c
+++ pthreads/main.c
@@ -1,7 +1,8 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS 1
 #define _WINSOCK_DEPRECATED_NO_WARNINGS 1
-
+#pragma comment(lib,"pthreadVC2.lib") 
+#define HAVE_STRUCT_TIMESPEC 
 #include <pthread.h>
 #include <stdio.h>
 #include <math.h>
@@ -9,11 +10,29 @@
 #include <time.h>
 
 #define NUM_MAX 31999
-#define COLUNAS 10000
-#define LINHAS 10000
+#define ALTURA 10000
+#define LARGURA 9000
+
+#define ALTURA_MACRO_BLOCO 5000
+#define LARGURA_MACRO_BLOCO 3000
 
 #define SEED 67
 
+#define NUM_THREADS 6
+
+int** matriz;
+
+int cont = 0;
+
+int quantidade_blocos;
+
+int linhas_bloco;
+int colunas_bloco;
+
+int proximo_bloco = 1;
+
+pthread_mutex_t cont_mutex;
+pthread_mutex_t bloco_mutex;
 
 int** aloca_matriz(int m, int n) {
 	int** matriz;
@@ -62,20 +81,6 @@ void preenche_matriz(int m, int n, int** matriz) {
 	}
 }
 	
-
-int main() {
-	srand(SEED);
-	int** matriz = aloca_matriz(LINHAS, COLUNAS);
-
-	preenche_matriz(LINHAS, COLUNAS, matriz);
-	
-	pthread_t tid;
-
-	matriz = liberar_matriz(LINHAS, COLUNAS, matriz);
-	return 0;
-}
-
-
 int eh_primo(int n) {
 	if (n <= 1) return 0;
 
@@ -85,14 +90,115 @@ int eh_primo(int n) {
 	return 1;
 }
 
-int busca_serial(int** matriz) {
-	int count = 0;
-	for (int i = 0; i < LINHAS; i++) {
-		for (int j = 0; j < COLUNAS; j++) {
+void busca_serial() {
+	for (int i = 0; i < ALTURA; i++) {
+		for (int j = 0; j < LARGURA; j++) {
 			if (eh_primo(matriz[i][j])) {
-				count++;
+				cont++;
 			}
 		}
 	}
-	return count;
+}
+
+
+void *thread_function(void* nenhum) {
+
+	int primos_local = 0;
+	int bloco_atual;
+
+	while (1){
+
+		pthread_mutex_lock(&bloco_mutex);
+		if (proximo_bloco > quantidade_blocos) {
+			pthread_mutex_unlock(&bloco_mutex);
+			break;
+		}
+		bloco_atual = proximo_bloco;
+		proximo_bloco++;
+		pthread_mutex_unlock(&bloco_mutex);
+
+		// TA ERRADO DE ALGUMA FORMA N FAÇO IDEIA DO QUE FAZER SERA QUE SOU BURRO?
+		int linha = (bloco_atual - 1) / linhas_bloco;
+		int coluna = (bloco_atual - 1) % colunas_bloco;
+
+		int linha_inicial = linha * ALTURA_MACRO_BLOCO;
+		int linha_final = linha_inicial + (ALTURA_MACRO_BLOCO - 1);
+
+		int coluna_inicial = coluna * LARGURA_MACRO_BLOCO;
+		int coluna_final = coluna_inicial + (LARGURA_MACRO_BLOCO - 1);
+
+		for (int i = linha_inicial; i <= linha_final; i++) {
+			for (int j = coluna_inicial; j <= coluna_final; j++) {
+				if (eh_primo(matriz[i][j])) {
+					primos_local++;
+				}
+			}
+		}
+
+		pthread_mutex_lock(&cont_mutex);
+		cont += primos_local;
+		pthread_mutex_unlock(&cont_mutex);
+		primos_local = 0;
+	}
+
+	pthread_exit(NULL);
+}
+
+void busca_paralela() {
+	pthread_t threads[NUM_THREADS];
+	cont = 0;
+
+	linhas_bloco = (int)(ceil((double)ALTURA / ALTURA_MACRO_BLOCO));
+	colunas_bloco = (int)(ceil((double)LARGURA / LARGURA_MACRO_BLOCO));
+
+	pthread_mutex_init(&cont_mutex, NULL);
+	pthread_mutex_init(&bloco_mutex, NULL);
+
+	for (int i = 0; i < NUM_THREADS; i++) {
+		if (pthread_create(&threads[i], NULL, thread_function, NULL) != 0) {
+			perror("Pthread_create falhou");
+			exit(1);
+		}
+	}
+
+	for (int i = 0; i < NUM_THREADS; i++) {
+		if (pthread_join(threads[i], NULL)) {
+			perror("Pthread_join falhou");
+			exit(1);
+		}
+	}
+
+	pthread_mutex_destroy(&cont_mutex);
+	pthread_mutex_destroy(&bloco_mutex);
+
+}
+
+
+int main() {
+	srand(SEED);
+	matriz = aloca_matriz(ALTURA, LARGURA);
+
+	quantidade_blocos = (int)(ceil((double)ALTURA / ALTURA_MACRO_BLOCO) * ceil((double)LARGURA / LARGURA_MACRO_BLOCO));
+
+	preenche_matriz(ALTURA, LARGURA, matriz);
+	clock_t clock_inicio, clock_fim;
+	clock_inicio = clock();
+	busca_serial();
+	clock_fim = clock();
+	double tempo_serial = ((double)(clock_fim - clock_inicio)) / CLOCKS_PER_SEC;
+	printf("Busca Serial: %d numeros primos encontrados em %lf segundos.\n", cont, tempo_serial);
+	
+
+	clock_inicio = clock();
+	busca_paralela();
+	clock_fim = clock();
+	double tempo_paralelo = ((double)(clock_fim - clock_inicio)) / CLOCKS_PER_SEC;
+	printf("Busca Paralela: %d numeros primos encontrados em %lf segundos.\n", cont, tempo_paralelo);
+	printf("Realizada com %d threads e %d Macro Blocos.\n", NUM_THREADS, quantidade_blocos);
+	matriz = liberar_matriz(ALTURA, LARGURA, matriz);
+	double speedup = tempo_serial / tempo_paralelo;
+	printf("Speedup: %lf\n", speedup);
+
+
+	return 0;
 }
